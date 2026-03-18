7eb8a542f8082c5ca982df8297eae16137827792
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-18 17:06:21 +0200

    Styled answering and splitted to separate components

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,6 +4,7 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
+    console.log('SETTING TIMEOUT');
     const timer = setTimeout(onTimeout, timeout);
 
     return () => {
@@ -12,6 +13,7 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   }, [timeout, onTimeout]);
 
   useEffect(() => {
+    console.log('SETTING INTERVAL');
     const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
