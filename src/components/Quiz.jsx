b6492e493d183be83cc46ec2561532d39b410e23
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-18 17:29:35 +0200

    Refactor: improve logic for answering UI

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback } from "react";
 
-import QUESTIONS from '../questions.js';
-import quizCompleteImg from '../assets/quiz-complete.png';
-import Question from './Question.jsx';
+import QUESTIONS from "../questions.js";
+import Question from "./Question.jsx";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -11,17 +11,16 @@ export default function Quiz() {
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    selectedAnswer
+    selectedAnswer,
   ) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-  },
-  []);
+  }, []);
 
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
-    [handleSelectAnswer]
+    [handleSelectAnswer],
   );
 
   if (quizIsComplete) {
